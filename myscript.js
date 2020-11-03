4f6d7bb5c07c7d16584c1d6722bba1d3e6f82e58
Tran Thanh Xuan <56567195+Avocado0595@users.noreply.github.com>
2020-11-03 21:17:06 +0700

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,51 @@
+//flating array
+function flatten(arr) {
+  // viết code ở đây.
+  return arr.reduce((res, item)=>{return res.concat(item)},[]);
+}
+
+
+function toFlat(arr)
+{
+  return arr.reduce(
+    (result, item)=>
+    {
+      result.concat(item);
+    }, []
+  )
+}
+
+
+//count frequence of element
+
+function frequenceElement(arr)
+{
+  return arr.reduce(
+    (result, item)=>{
+      if (isNaN(result[item])
+        result[item]=1;
+       else
+        result[item]+=1;
+       return result;
+    }, {}
+  )
+}
+
+function countOccurrences(arr) {
+  // viết code ở đây.
+  return arr.reduce((result, item)=>
+  {
+      if (isNaN(result[item]))
+        result[item] = 1;
+      else
+        result[item]+=1;
+        return result;
+      
+      
+  }, {})
+}
+//remove duplicate
+function removeDuplicate(array) {
+  // write code here...
+  return array.filter((value,index)=>array.indexOf(value) == index);
+}
