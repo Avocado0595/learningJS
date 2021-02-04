b1c28c999eb6ab86e9b9b0491c2d7a9dfc8be297
Tran Thanh Xuan <56567195+Avocado0595@users.noreply.github.com>
2021-02-04 20:56:41 +0700

    Create noteJS.js

---
+++ noteJS.js
@@ -0,0 +1,10 @@
+/*
+1. Null and Undefined
+- Undefined : 1 biến khai báo nhưng không gán giá trị, kiểu undefined, giá trị : null
+- Null: giá trị null, kiểu null
+ null == undefined => true
+ null === undefined => false
+ - NaN : not a number - lỗi khi xứ lý toán học với các giá trị không phải số
+ - typeof(NaN) = number
+ - Infinity/ -Infinity
+*/
